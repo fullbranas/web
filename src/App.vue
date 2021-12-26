b289e559ff997bad5e9c7ac58f154a0476a863c2
hotequil <hotequil@protonmail.com>
2021-12-26 11:45:27 -0300

    added bootstrap and font awesome, changed the favicon and removed components.

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,58 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.jpg">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="app">
+        <img class="app__image" src="./assets/logo.jpg" alt="fullbranas logo">
+        <h1 class="app__title">fullbranas <i aria-hidden="true" class="fa fa-tint"></i></h1>
+        <p class="app__text">name generators with NodeJS, GraphQL and VueJS.</p>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import "bootstrap/dist/css/bootstrap.min.css";
+    import "font-awesome/css/font-awesome.min.css";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'App'
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    :root{
+        --space: 16px;
+        --double-space: calc(var(--space) * 2);
+        --primary: #651FFF;
+    }
+
+    html, body{
+        font-size: var(--space);
+    }
+
+    .app{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+        margin-top: var(--double-space);
+        row-gap: var(--space);
+    }
+
+    .app .app__image{
+        width: 200px;
+    }
+
+    .app .app__title, .app .app__text{
+        margin: 0;
+        text-align: center;
+    }
+
+    .app .app__text{
+        font-size: 1rem;
+    }
+
+    .app .app__title{
+        font-size: 3rem;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        column-gap: var(--space);
+    }
 </style>
