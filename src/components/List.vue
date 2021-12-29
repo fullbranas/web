57132205b8d4d87a9f5bbf309fad7abe5e1f133a
hotequil <hotequil@protonmail.com>
2021-12-28 21:39:45 -0300

    created a List component.

---
+++ src/components/List.vue
@@ -0,0 +1,48 @@
+<template>
+    <ul class="card list">
+        <li v-for="(item, index) in items" v-bind:key="domain ? item.name : item" class="card">
+            <span class="card-body list__item">
+                {{ domain ? item.name : item }}
+
+                <a v-if="domain" v-bind:href="item.link" target="_blank" class="btn btn-info" aria-label="shopping">
+                    <i aria-hidden="true" class="fa fa-shopping-cart"></i>
+                </a>
+
+                <button v-else @click="destroy(index)" class="btn btn-danger" aria-label="delete" type="button">
+                    <i aria-hidden="true" class="fa fa-trash"></i>
+                </button>
+            </span>
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+    name: "List",
+    props: ["items", "domain"],
+    methods: {
+        destroy(index){
+            this.$emit("destroy", index);
+        }
+    }
+};
+</script>
+
+<style scoped>
+    .list{
+        padding: 0;
+        margin: 0;
+        display: flex;
+        flex-direction: column;
+        background-color: transparent;
+        border: none;
+        row-gap: var(--half-space);
+        width: 100%;
+    }
+
+    .list .list__item{
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+</style>
